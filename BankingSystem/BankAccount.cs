415d28cad55e5a42c1088f17c56fe1717994b202
DilshanMadhusanka <139458721+DilshanMadhusanka@users.noreply.github.com>
2023-11-09 23:41:51 +0530

    Add project files.

---
+++ BankingSystem/BankAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem
+{
+ public class BankAccount
+    {
+        /*
+         
+        The features that should include
+         
+        it has 10 digit numbers that uniquely identifies the bank account
+        it has a string that stores the name or names of the owner
+        the balanced can be retrieved
+        it accepts deposits
+        it accepts withdrawls
+         => the initial balance must be positive
+         => withdrawal can't result in a negative balance
+        
+         */
+
+        /* definet the properties and get set method */
+
+        public String Number { get;  }
+        public String Owner { get; set; }
+        public decimal Balance { get; }
+
+        // methana get krala witharak thiyan properties walata class eka athukenma set property eka set krann oni. ekata constructor eka hadanwa
+
+
+        // Constructor 
+
+        public BankAccount( String name , decimal initialBalance)
+        {
+           this.Owner = name;
+           this.Balance = initialBalance;
+        }
+
+
+
+        public void MakeDeposite (decimal amount , DateTime date , String note)
+        { }
+
+        public void MakeWithdrawal (decimal amount , DateTime date , String note) 
+        { }
+
+    }
+}
---
+++ BankingSystem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankingSystem;
+
+namespace BankingSystem
+{
+  public class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account1 = new BankAccount( "DILSHAN " , 5000);  // new key word eka dala object ekak hadankot automaticaly constructor is called.
+            
+            // Console.WriteLine(account1.Owner );
+            // Console.WriteLine(account1.Balance);
+            // Console.ReadLine(); 
+          
+        }
+    }
+}
