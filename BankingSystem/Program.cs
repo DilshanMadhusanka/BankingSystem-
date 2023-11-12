88a08760af8b1bdc225c1b046aa1c0d3a5c7b266
DilshanMadhusanka <139458721+DilshanMadhusanka@users.noreply.github.com>
2023-11-12 21:52:03 +0530

    create an account

--- BankingSystem/Program.cs
+++ BankingSystem/Program.cs
@@ -13,6 +13,7 @@ namespace BankingSystem
         {
             BankAccount account1 = new BankAccount( "DILSHAN " , 5000);  // new key word eka dala object ekak hadankot automaticaly constructor is called.
             BankAccount account2 = new BankAccount("kamal", 35000);
+           // BankAccount account3 = new BankAccount("Ahinsa", 33434);
 
             // Console.WriteLine(account1.Owner );
             // Console.WriteLine(account1.Balance);
@@ -20,9 +21,14 @@ namespace BankingSystem
 
             Console.WriteLine($"This account is for {account1.Owner} and the number is {account1.Number}");
             Console.WriteLine($"This account is for {account2.Owner} and the number is {account2.Number}");
+           // Console.WriteLine($" this account is for {account3.Owner} and the number is {account3.Number}");
+
+
             Console.ReadLine();
 
 
+
+           
 
         }
     }
